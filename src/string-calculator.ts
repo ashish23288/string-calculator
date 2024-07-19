ee768d9233474c6dcef8af2579edc0f753d46814
ashish <ashish.pratap34@gmail.com>
2024-07-19 16:51:40 +0530

    test for two numbers

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,4 +1,6 @@
 export function add(input: string): number {
-  if (input === "") return 0;
-  return parseInt(input);
+  if (input === '') return 0;
+
+  const numbers = input.split(',').map(num => parseInt(num, 10));
+  return numbers.reduce((sum, current) => sum + current, 0);
 }
--- test/string-calculator.test.ts
+++ test/string-calculator.test.ts
@@ -8,4 +8,9 @@ describe("StringCalculator", () => {
   test("should return the number itself when a single number is provided", () => {
     expect(add("1")).toBe(1);
   });
+
+  test('should return the sum of two comma-separated numbers', () => {
+    expect(add('1,2')).toBe(3);
+  });
+  
 });
