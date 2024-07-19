56e916e5db35d49463fd9061603eb4783cdf30dc
ashish <ashish.pratap34@gmail.com>
2024-07-19 16:48:32 +0530

    test for empty string and single number

---
+++ test/string-calculator.test.ts
@@ -0,0 +1,11 @@
+import { add } from "../src/string-calculator";
+
+describe("StringCalculator", () => {
+  test("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+
+  test("should return the number itself when a single number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
+});
